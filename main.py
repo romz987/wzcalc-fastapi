efa64ea001594fd2f277b4a7d7f0d3aea28399bd
romz987pc <eesistance@gmail.com>
2025-08-28 14:18:02 +0300

    feat: 1. add signup test endpoint;  2. add UserInfo test pydantic model;

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from typing import Union
 
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+
+
+class UserInfo(BaseModel):
+    name: str
+    email: str
+
+
 app = FastAPI()
 
 
@@ -13,3 +21,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.post("/signup")
+def register_user(payload: UserInfo):
+    return {"user_name": payload.name, "user_email": payload.email}
