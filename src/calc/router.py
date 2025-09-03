026bb25862d19ea98cf206139be9c702e4963259
romz987pc <eesistance@gmail.com>
2025-09-03 21:49:58 +0300

    feat: 1. add services for FBS & FBO logistics routes;  2. add services for FBS & FBO returns routes;

--- src/calc/router.py
+++ src/calc/router.py
@@ -1,18 +1,11 @@
 from fastapi import APIRouter
-from .schemas import (
-    # Ozon
-    OzonFbsPayload,
-    OzonFboPayload,
-    OzonLogFbsPayload,
-    OzonLogFboPayload,
-    OzonReturnsFbsPayload,
-    OzonReturnsFboPayload,
-    # Wildberries
-    WbPayload,
-    WbLogPayload,
-    WbReturnsPayload,
+from . import schemas
+from .service import (
+    calculate_logistics_fbs_oz,
+    calculate_logistics_fbo_oz,
+    calculate_returns_fbs_oz,
+    calculate_returns_fbo_oz,
 )
-
 
 router = APIRouter()
 
@@ -24,50 +17,50 @@ router = APIRouter()
 
 # Единичный расчет цены fbs
 @router.post("/ozon/prices/fbs/calculate")
-def ozon_prices_fbs_calc(payload: OzonFbsPayload):
+def ozon_prices_fbs_calc(payload: schemas.OzonFbsPayload):
     return {"ok": "it works!"}
 
 
 # Единичный расчет цены fbo
 @router.post("/ozon/prices/fbo/calculate")
-def ozon_prices_fbo_calc(payload: OzonFboPayload):
+def ozon_prices_fbo_calc(payload: schemas.OzonFboPayload):
     return {"ok": "it works!"}
 
 
 # Массовый расчет цен fbs
 @router.post("/ozon/prices/fbs/bulk/calculate/")
-def ozon_prices_fbs_bulk_calc(payloads: list[OzonFbsPayload]):
+def ozon_prices_fbs_bulk_calc(payloads: list[schemas.OzonFbsPayload]):
     return {"ok": "it works!"}
 
 
 # Массовый расчет цен fbo
 @router.post("/ozon/prices/fbo/bulk/calculate/")
-def ozon_prices_fbo_bulk_calc(payloads: list[OzonFboPayload]):
+def ozon_prices_fbo_bulk_calc(payloads: list[schemas.OzonFboPayload]):
     return {"ok": "it works!"}
 
 
 # Расчет стоимости логистики fbs
 @router.post("/ozon/logistics/fbs/calculate")
-def ozon_logistics_fbs_calc(payload: OzonLogFbsPayload):
-    return {"ok": "it works!"}
+def ozon_logistics_fbs_calc(payload: schemas.OzonLogFbsPayload):
+    return calculate_logistics_fbs_oz(payload)
 
 
 # Расчет стоимости логистики fbo
 @router.post("/ozon/logistics/fbo/calculate")
-def ozon_logistics_fbo_calc(payload: OzonLogFboPayload):
-    return {"ok": "it works!"}
+def ozon_logistics_fbo_calc(payload: schemas.OzonLogFboPayload):
+    return calculate_logistics_fbo_oz(payload)
 
 
 # Расчет стоимости возвратов fbs
 @router.post("/ozon/returns/fbs/calculate")
-def ozon_returns_fbs_calc(payload: OzonReturnsFbsPayload):
-    return {"ok": "it works!"}
+def ozon_returns_fbs_calc(payload: schemas.OzonReturnsFbsPayload):
+    return calculate_returns_fbs_oz(payload)
 
 
 # Расчет стоимости возвратов fbo
 @router.post("/ozon/returns/fbo/calculate")
-def ozon_returns_fbo_calc(payload: OzonReturnsFboPayload):
-    return {"ok": "it works!"}
+def ozon_returns_fbo_calc(payload: schemas.OzonReturnsFboPayload):
+    return calculate_returns_fbo_oz(payload)
 
 
 ##########################################################
@@ -77,23 +70,23 @@ def ozon_returns_fbo_calc(payload: OzonReturnsFboPayload):
 
 # Единичный расчет цены
 @router.post("/wb/prices/calculate")
-def wb_prices_fbs_calc(payload: WbPayload):
+def wb_prices_fbs_calc(payload: schemas.WbPayload):
     return {"ok": "it works!"}
 
 
 # Массовый расчет цен
 @router.post("/wb/prices/bulk/calculate/")
-def wb_prices_bulk_calc(payloads: list[WbPayload]):
+def wb_prices_bulk_calc(payloads: list[schemas.WbPayload]):
     return {"ok": "it works!"}
 
 
 # Расчет стоимости логистики
 @router.post("/wb/logistics/calculate")
-def wb_logistics_calc(payload: WbLogPayload):
+def wb_logistics_calc(payload: schemas.WbLogPayload):
     return {"ok": "it works!"}
 
 
 # Расчет стоимости возвратов
 @router.post("/wb/returns/calculate")
-def wb_returns_calc(payload: WbReturnsPayload):
+def wb_returns_calc(payload: schemas.WbReturnsPayload):
     return {"ok": "it works!"}
