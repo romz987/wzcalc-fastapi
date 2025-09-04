5bb2d3ec6b76b2a7b9cb1af1d73c8f5531a1f86a
romz987pc <eesistance@gmail.com>
2025-09-04 20:37:22 +0300

    feat: create interfaces;

---
+++ src/calc/service/service_wb/logistics_wb.py
@@ -0,0 +1,49 @@
+def calculate_logistics_fbs_wb():
+    """Calculate FBO logistics cost for Wildberries
+
+    :param payload: JSON from request
+    :return: LogFbsData dataclass
+    """
+    # create args instance
+    # calculate box volume
+    # calculate fbs logistics fee
+    pass
+
+
+def calculate_logistics_fbo_wb():
+    """Calculate FBO logistics cost for Wildberries
+
+    :param payload: JSON from request
+    :return: LogFboData dataclass
+    """
+    # create args instance
+    # calculate box volume
+    # calculate fbs logistics fee
+    pass
+
+
+def calculate_returns_fbs_wb():
+    """Calculate FBS returns cost for Wildberries
+
+    :param payload: JSON from request
+    :return: ReturnsData dataclass
+    """
+    # create args instance
+    # calculate logistics fee
+    # calculate reverse logistics fee
+    # calculate returns
+    pass
+
+
+def calculate_returns_fbo_wb():
+    """Calculate FBO returns cost for Wildberries
+
+    :param payload: JSON from request
+    :return: ReturnsData dataclass
+    """
+    # create args instance
+    # calculate box volume
+    # calculate logistics fee
+    # calculate reverse logistics fee
+    # calculate returns
+    pass
