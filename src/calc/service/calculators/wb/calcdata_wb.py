ccb91a6c6eb640f212848a56e024658efd2268e2
romz987pc <eesistance@gmail.com>
2025-09-06 22:52:41 +0300

    feat: add wb returns dataclass and calculator;

--- src/calc/service/calculators/wb/calcdata_wb.py
+++ src/calc/service/calculators/wb/calcdata_wb.py
@@ -13,34 +13,21 @@ class LogWbData:
     logistics_fee: Decimal | None = None
 
 
-# @dataclass
-# class LogFboData:
-#     local_index: Decimal
-#     box_size: str
-#     base_price_fbo: Decimal
-#     volume_factor_fbo: Decimal
-#     fix_large_fbo: Decimal
-#     # flowing
-#     box_volume: Decimal | None = None
-#     logistics_fee: Decimal | None = None
-#
-#
-# @dataclass
-# class ReturnsFbsData:
-#     box_size: str
-#     redemption_percentage: Decimal
-#     nonredemption_processing_cost: Decimal
-#     local_index: Decimal
-#     # logistics costs
-#     minimal_price_fbs: Decimal
-#     base_price_fbs: Decimal
-#     volume_factor_fbs: Decimal
-#     fix_large_fbs: Decimal
-#     # flowing
-#     box_volume: Decimal | None = None
-#     logistics_fee: Decimal | None = None
-#     reverse_logistics_fee: Decimal | None = None
-#     returns_fee: Decimal | None = None
+@dataclass
+class ReturnsWbData:
+    box_size: str
+    redemption_percentage: Decimal
+    nonredemption_processing_cost: Decimal
+    local_index: Decimal
+    # logistics costs
+    base_price: Decimal
+    volume_factor: Decimal
+    # flowing
+    box_volume: Decimal | None = None
+    logistics_fee: Decimal | None = None
+    returns_fee: Decimal | None = None
+
+
 #
 #
 # @dataclass
--- src/calc/service/calculators/wb/logistics_wb.py
+++ src/calc/service/calculators/wb/logistics_wb.py
@@ -28,3 +28,27 @@ def calc_log_wb(args: calcdata_wb.LogWbData) -> Decimal:
             additional_volume_price + args.base_price
         ) * args.local_index
     return logistics_fee  # pyright: ignore
+
+
+def calc_returns_wb(args: calcdata_wb.ReturnsWbData) -> Decimal:
+    """Ozon returns fee calculator based on:
+    - redemption_percentage
+    - nonredemption_processing_cost
+    - logistics_fee
+
+    :param args: An instance of ReturnsData
+    :return: Returns fee
+    """
+    # only for pyright alerts
+    if args.logistics_fee is None:
+        raise ValueError
+    # calculate all nonredemptions processing cost
+    nonredemptions_value = (
+        100 * args.logistics_fee
+        + (
+            (100 - args.redemption_percentage)
+            * args.nonredemption_processing_cost
+        )
+    ) / args.redemption_percentage
+
+    return nonredemptions_value - args.logistics_fee
