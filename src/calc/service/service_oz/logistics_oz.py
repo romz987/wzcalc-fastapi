6990f46719657c39286c430217e9f8a7f10491b7
romz987pc <eesistance@gmail.com>
2025-09-06 12:57:57 +0300

    fix: 1. calcdata -> calcdata_oz;  2. functions names;

--- src/calc/service/service_oz/logistics_oz.py
+++ src/calc/service/service_oz/logistics_oz.py
@@ -1,24 +1,24 @@
 from decimal import Decimal, ROUND_UP
 from src.calc import schemas
-from src.calc.service.calculators import calcdata
+from src.calc.service.calculators.oz import calcdata_oz
 from src.calc.service.calculators.common import get_box_volume
 from src.calc.service.calculators.oz.logistics_oz import (
-    calc_log_fbs,
-    calc_log_fbo,
-    calc_returns,
+    calc_log_fbs_oz,
+    calc_log_fbo_oz,
+    calc_returns_oz,
 )
 
 
 def calculate_logistics_fbs_oz(
     payload: schemas.OzonLogFbsPayload,
-) -> calcdata.LogFbsData:
-    """Calculate FBS logistics cost for Ozon
+) -> calcdata_oz.LogFbsData:
+    """Calculate FBS logistics fee for Ozon
 
     :param payload: JSON from request
     :return: LogFbsData dataclass
     """
     # create args instance
-    args = calcdata.LogFbsData(
+    args = calcdata_oz.LogFbsData(
         local_index=payload.local_index,
         box_size=payload.box_size,
         minimal_price_fbs=payload.minimal_price_fbs,
@@ -30,7 +30,7 @@ def calculate_logistics_fbs_oz(
     box_volume = get_box_volume(args.box_size)  # pyright: ignore
     args.box_volume = box_volume.quantize(Decimal("0.0"), rounding=ROUND_UP)
     # calculate fbs logistics fee
-    logistics_fee = calc_log_fbs(args)
+    logistics_fee = calc_log_fbs_oz(args)
     args.logistics_fee = logistics_fee.quantize(
         Decimal("0"),
         rounding=ROUND_UP,
@@ -40,14 +40,14 @@ def calculate_logistics_fbs_oz(
 
 def calculate_logistics_fbo_oz(
     payload: schemas.OzonLogFboPayload,
-) -> calcdata.LogFboData:
-    """Calculate FBO logistics cost for Ozon
+) -> calcdata_oz.LogFboData:
+    """Calculate FBO logistics fee for Ozon
 
     :param payload: JSON from request
     :return: LogFboData dataclass
     """
     # create args instance
-    args = calcdata.LogFboData(
+    args = calcdata_oz.LogFboData(
         local_index=payload.local_index,
         box_size=payload.box_size,
         base_price_fbo=payload.base_price_fbo,
@@ -58,7 +58,7 @@ def calculate_logistics_fbo_oz(
     box_volume = get_box_volume(args.box_size)  # pyright: ignore
     args.box_volume = box_volume.quantize(Decimal("0.0"), rounding=ROUND_UP)
     # calculate fbs logistics fee
-    logistics_fee = calc_log_fbo(args)
+    logistics_fee = calc_log_fbo_oz(args)
     args.logistics_fee = logistics_fee.quantize(
         Decimal("0"),
         rounding=ROUND_UP,
@@ -68,14 +68,14 @@ def calculate_logistics_fbo_oz(
 
 def calculate_returns_fbs_oz(
     payload: schemas.OzonReturnsFbsPayload,
-) -> calcdata.ReturnsFbsData:
-    """Calculate FBS returns cost for Ozon
+) -> calcdata_oz.ReturnsFbsData:
+    """Calculate FBS returns fee for Ozon
 
     :param payload: JSON from request
     :return: ReturnsFbsData dataclass
     """
     # create args instance
-    args = calcdata.ReturnsFbsData(
+    args = calcdata_oz.ReturnsFbsData(
         box_size=payload.box_size,
         redemption_percentage=payload.redemption_percentage,
         nonredemption_processing_cost=payload.nonredemption_processing_cost,
@@ -89,30 +89,30 @@ def calculate_returns_fbs_oz(
     box_volume = get_box_volume(args.box_size)  # pyright: ignore
     args.box_volume = box_volume.quantize(Decimal("0.0"), rounding=ROUND_UP)
     # calculate logistics fee
-    args.logistics_fee = calc_log_fbs(args)
+    args.logistics_fee = calc_log_fbs_oz(args)
     # calculate reverse logistics fee
     current_local_index, args.local_index = (  # pyright: ignore
         args.local_index,
         1,
     )
-    args.reverse_logistics_fee = calc_log_fbs(args)
+    args.reverse_logistics_fee = calc_log_fbs_oz(args)
     args.local_index = current_local_index
     # calculate returns
-    returns_fee = calc_returns(args)
+    returns_fee = calc_returns_oz(args)
     args.returns_fee = returns_fee.quantize(Decimal("0.0"), rounding=ROUND_UP)
     return args
 
 
 def calculate_returns_fbo_oz(
     payload: schemas.OzonReturnsFboPayload,
-) -> calcdata.ReturnsFboData:
-    """Calculate FBO returns cost for Ozon
+) -> calcdata_oz.ReturnsFboData:
+    """Calculate FBO returns fee for Ozon
 
     :param payload: JSON from request
     :return: ReturnsFboData dataclass
     """
     # create args instance
-    args = calcdata.ReturnsFboData(
+    args = calcdata_oz.ReturnsFboData(
         box_size=payload.box_size,
         redemption_percentage=payload.redemption_percentage,
         nonredemption_processing_cost=payload.nonredemption_processing_cost,
@@ -129,15 +129,15 @@ def calculate_returns_fbo_oz(
     box_volume = get_box_volume(args.box_size)  # pyright: ignore
     args.box_volume = box_volume.quantize(Decimal("0.0"), rounding=ROUND_UP)
     # calculate logistics fee
-    args.logistics_fee = calc_log_fbs(args)
+    args.logistics_fee = calc_log_fbs_oz(args)
     # calculate reverse logistics fee
     current_local_index, args.local_index = (  # pyright: ignore
         args.local_index,
         1,
     )
-    args.reverse_logistics_fee = calc_log_fbs(args)
+    args.reverse_logistics_fee = calc_log_fbs_oz(args)
     args.local_index = current_local_index
     # calculate returns
-    returns_fee = calc_returns(args)
+    returns_fee = calc_returns_oz(args)
     args.returns_fee = returns_fee.quantize(Decimal("0.0"), rounding=ROUND_UP)
     return args
