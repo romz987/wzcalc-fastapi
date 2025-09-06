517e8d7dbf78bc64d31b570153fef15256ac378e
romz987pc <eesistance@gmail.com>
2025-09-06 12:56:31 +0300

    feat: 1. add calculate_logistics_wb;  2. add check_local_index with 422 response;  3. add calculate_returns_wb plug;

--- src/calc/service/service_wb/logistics_wb.py
+++ src/calc/service/service_wb/logistics_wb.py
@@ -1,29 +1,41 @@
-def calculate_logistics_fbs_wb():
-    """Calculate FBO logistics cost for Wildberries
+from fastapi import HTTPException
+from decimal import ROUND_UP, Decimal
+from src.calc.service.calculators.common import get_box_volume
+from src.calc.service.calculators.wb.logistics_wb import calc_log_wb
+from src.calc import schemas
+from src.calc.service.calculators.wb import calcdata_wb
+
+
+def calculate_logistics_wb(payload: schemas.WbLogPayload):
+    """Calculate logistics fee for Wildberries.
+    Depends on check_local_index()
 
     :param payload: JSON from request
     :return: LogFbsData dataclass
     """
+    # check fbs local_index is correct
+    check_local_index(payload)
     # create args instance
+    args = calcdata_wb.LogWbData(
+        box_size=payload.box_size,
+        local_index=payload.local_index,
+        base_price=payload.base_price,
+        volume_factor=payload.volume_factor,
+    )
     # calculate box volume
-    # calculate fbs logistics fee
-    pass
+    box_volume = get_box_volume(args.box_size)
+    args.box_volume = box_volume.quantize(Decimal("0.0"), rounding=ROUND_UP)
+    # calculate logistics fee
+    logistics_fee = calc_log_wb(args)
+    args.logistics_fee = logistics_fee.quantize(
+        Decimal("0"),
+        rounding=ROUND_UP,
+    )
+    return args
 
 
-def calculate_logistics_fbo_wb():
-    """Calculate FBO logistics cost for Wildberries
-
-    :param payload: JSON from request
-    :return: LogFboData dataclass
-    """
-    # create args instance
-    # calculate box volume
-    # calculate fbs logistics fee
-    pass
-
-
-def calculate_returns_fbs_wb():
-    """Calculate FBS returns cost for Wildberries
+def calculate_returns_wb(payload):
+    """Calculate returns fee for Wildberries
 
     :param payload: JSON from request
     :return: ReturnsData dataclass
@@ -32,18 +44,17 @@ def calculate_returns_fbs_wb():
     # calculate logistics fee
     # calculate reverse logistics fee
     # calculate returns
-    pass
+    return {"answer": "it works!"}
 
 
-def calculate_returns_fbo_wb():
-    """Calculate FBO returns cost for Wildberries
+def check_local_index(payload: schemas.WbLogPayload) -> HTTPException | None:
+    """Check local_index value in accordance with FBS/FBO option
 
     :param payload: JSON from request
-    :return: ReturnsData dataclass
+    :return: Error message or None
     """
-    # create args instance
-    # calculate box volume
-    # calculate logistics fee
-    # calculate reverse logistics fee
-    # calculate returns
-    pass
+    if payload.fbs_fbo_option == "fbs" and payload.local_index != 1:
+        raise HTTPException(
+            status_code=422,
+            detail="local_index must be == 1 for FBS logistics option",
+        )
