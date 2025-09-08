196e83132a70ac201d1ae7921b6911098a5290c1
romz987pc <eesistance@gmail.com>
2025-09-08 10:43:56 +0300

    feat:  1. add calculate_returns_wb logic;  2. add type annotations;

--- src/calc/service/service_wb/logistics_wb.py
+++ src/calc/service/service_wb/logistics_wb.py
@@ -1,12 +1,17 @@
 from fastapi import HTTPException
 from decimal import ROUND_UP, Decimal
 from src.calc.service.calculators.common import get_box_volume
-from src.calc.service.calculators.wb.logistics_wb import calc_log_wb
+from src.calc.service.calculators.wb.logistics_wb import (
+    calc_log_wb,
+    calc_returns_wb,
+)
 from src.calc import schemas
 from src.calc.service.calculators.wb import calcdata_wb
 
 
-def calculate_logistics_wb(payload: schemas.WbLogPayload):
+def calculate_logistics_wb(
+    payload: schemas.WbLogPayload,
+) -> calcdata_wb.LogWbData:
     """Calculate logistics fee for Wildberries.
     Depends on check_local_index()
 
@@ -34,20 +39,41 @@ def calculate_logistics_wb(payload: schemas.WbLogPayload):
     return args
 
 
-def calculate_returns_wb(payload):
+def calculate_returns_wb(payload: schemas.WbReturnsPayload):
     """Calculate returns fee for Wildberries
 
     :param payload: JSON from request
     :return: ReturnsData dataclass
     """
+    # check fbs local_index is correct
+    check_local_index(payload)
     # create args instance
+    args = calcdata_wb.ReturnsWbData(
+        box_size=payload.box_size,
+        redemption_percentage=payload.redemption_percentage,
+        nonredemption_processing_cost=payload.nonredemption_processing_cost,
+        local_index=payload.local_index,
+        base_price=payload.base_price,
+        volume_factor=payload.volume_factor,
+    )
+    # calculate box volume
+    box_volume = get_box_volume(args.box_size)
+    args.box_volume = box_volume.quantize(Decimal("0.0"), rounding=ROUND_UP)
     # calculate logistics fee
-    # calculate reverse logistics fee
+    logistics_fee = calc_log_wb(args)
+    args.logistics_fee = logistics_fee.quantize(
+        Decimal("0.0"),
+        rounding=ROUND_UP,
+    )
     # calculate returns
-    return {"answer": "it works!"}
+    returns_fee = calc_returns_wb(args)
+    args.returns_fee = returns_fee.quantize(Decimal("0.0"), rounding=ROUND_UP)
+    return args
 
 
-def check_local_index(payload: schemas.WbLogPayload) -> HTTPException | None:
+def check_local_index(
+    payload: schemas.WbLogPayload | schemas.WbReturnsPayload,
+) -> HTTPException | None:
     """Check local_index value in accordance with FBS/FBO option
 
     :param payload: JSON from request
