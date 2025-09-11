58a464c8708674a685862481ef653c1000735b67
romz987pc <eesistance@gmail.com>
2025-09-11 12:50:04 +0300

    fix: comments, docs, type annotations and func names;

--- src/calc/core/services/oz_services.py
+++ src/calc/core/services/oz_services.py
@@ -20,22 +20,22 @@ from src.calc.core.calculators.ozon import (
 
 
 def oz_calculate_returns_fbs(
-    item_params: cm_calcdata.LogMainParams,
+    log_params: cm_calcdata.LogMainParams,
     log_costs: oz_calcdata.OzLogFbsCosts,
     return_params: cm_calcdata.ReturnsParams,
 ) -> tuple[Decimal, Decimal, Decimal]:
     """Ozon FBS returns fee value calculator
 
-    :param item_params: An instance of LogMainParams dataclass
+    :param log_params: An instance of LogMainParams dataclass
     :param log_costs: An instance of OzLogFbsCosts dataclass
     :param return_params: An instance of ReturnsParams dataclass
     :return: tuple(logistics_fee, reverse_logistics_fee, returns_fee)
     """
     # Calculate logistics fee
-    logistics_fee = oz_log_fbs_clc(item_params, log_costs)
+    logistics_fee = oz_log_fbs_clc(log_params, log_costs)
     # Calculate reverse logistics fee
     reverse_logistics_fee = oz_calculate_reverse_logistics_fee(
-        item_params,
+        log_params,
         log_costs,
     )
     # Calculate returns_fee
@@ -48,24 +48,24 @@ def oz_calculate_returns_fbs(
 
 
 def oz_calculate_returns_fbo(
-    item_params: cm_calcdata.LogMainParams,
+    log_params: cm_calcdata.LogMainParams,
     log_costs_fbs: oz_calcdata.OzLogFbsCosts,
     log_costs_fbo: oz_calcdata.OzLogFboCosts,
     return_params: cm_calcdata.ReturnsParams,
 ) -> tuple[Decimal, Decimal, Decimal]:
     """Ozon FBO returns fee value calculator
 
-    :param item_params: An instance of LogMainParams dataclass
+    :param log_params: An instance of LogMainParams dataclass
     :param log_costs_fbs: An instance of OzLogFbsCosts dataclass
     :param log_costs_fbo: An instance of OzLogFboCosts dataclass
     :param return_params: An instance of ReturnsParams dataclass
     :return: tuple(logistics_fee, reverse_logistics_fee, returns_fee)
     """
     # Calculate logistics fee
-    logistics_fee = oz_log_fbo_clc(item_params, log_costs_fbo)
+    logistics_fee = oz_log_fbo_clc(log_params, log_costs_fbo)
     # Calculate reverse logistics fee
     reverse_logistics_fee = oz_calculate_reverse_logistics_fee(
-        item_params,
+        log_params,
         log_costs_fbs,
     )
     # Calculate returns_fee
@@ -78,17 +78,17 @@ def oz_calculate_returns_fbo(
 
 
 def oz_calculate_reverse_logistics_fee(
-    item_params: cm_calcdata.LogMainParams,
+    log_params: cm_calcdata.LogMainParams,
     log_costs: oz_calcdata.OzLogFbsCosts,
 ) -> Decimal:
     """Ozon reverse logistics fee value calculator
 
-    :param item_params: An instance of LogMainParams dataclass
+    :param log_params: An instance of LogMainParams dataclass
     :param log_costs: An instance of OzLogFbsCosts
     :return: Reverse logistics fee value
     """
-    reverse_log_item_params = replace(item_params, local_index=Decimal("1"))
-    return oz_log_fbs_clc(reverse_log_item_params, log_costs)
+    reverse_log_params = replace(log_params, local_index=Decimal("1"))
+    return oz_log_fbs_clc(reverse_log_params, log_costs)
 
 
 ################################# Profit ####################################
