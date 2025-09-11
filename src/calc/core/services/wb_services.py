7e133c6f19c93838a29dc5ea25a3079dc3542147
romz987pc <eesistance@gmail.com>
2025-09-11 12:48:02 +0300

    feat: add wb_calculate_returns func;

--- src/calc/core/services/wb_services.py
+++ src/calc/core/services/wb_services.py
@@ -1,3 +1,11 @@
+from decimal import Decimal
+
+# dataclasses
+from src.calc.core.domain import wb_calcdata, cm_calcdata
+
+# calculators
+from src.calc.core.calculators.wildberries import wb_log_clc, wb_returns_clc
+
 #############################################################################
 #                        Ozon Calculation Services                          #
 #############################################################################
@@ -5,15 +13,23 @@
 ################################# Returns ###################################
 
 
-def wb_calculate_returns():
+def wb_calculate_returns(
+    log_params: cm_calcdata.LogMainParams,
+    log_costs: wb_calcdata.WbLogCosts,
+    return_params: cm_calcdata.ReturnsParams,
+) -> tuple[Decimal, Decimal]:
     """Wildberries returns fee value calculator
 
-    :param :
-    :param :
-    :param :
+    :param log_params: An instance of LogMainParams dataclass
+    :param log_costs: An instance of WbLogCosts dataclass
+    :param return_params: An instance of ReturnsParams dataclass
     :return: tuple()
     """
-    pass
+    # Calculate logistics fee
+    logistics_fee = wb_log_clc(log_params, log_costs)
+    # Calculate returns fee
+    returns_fee = wb_returns_clc(return_params, logistics_fee)
+    return logistics_fee, returns_fee
 
 
 ################################# Profit ####################################
