dc3710d27e58932ecf7ef3643ffb1820c443714b
romz987pc <eesistance@gmail.com>
2025-09-11 14:05:32 +0300

    fix: remove 'check' old dir;

--- src/main.py
+++ src/main.py
@@ -1,10 +1,7 @@
 from fastapi import FastAPI
-from src.check.router import router as check_router
 from src.calc.router import router as calc_router
 
 app = FastAPI()
 
-# Регистрация endpoints check
-app.include_router(check_router, prefix="/check", tags=["check-app"])
 # Регистрация endpoints calc
 app.include_router(calc_router, prefix="/api/v1", tags=["calcs"])
