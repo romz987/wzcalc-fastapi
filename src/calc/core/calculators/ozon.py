dd77e033ecf6aeb3b0066b5f546b475cdc81ad98
romz987pc <eesistance@gmail.com>
2025-09-11 12:18:28 +0300

    fix: 1. fixes in accordance with new cm_calcdata module;  2. item_params -> log_params;  3. fix comments;

--- src/calc/core/calculators/ozon.py
+++ src/calc/core/calculators/ozon.py
@@ -1,10 +1,12 @@
 import math
 from decimal import Decimal
-from src.calc.core.domain import oz_calcdata
+
+# dataclasses
+from src.calc.core.domain import oz_calcdata, cm_calcdata
 
 
 def oz_log_fbs_clc(
-    item_params: oz_calcdata.OzLogItemParams,
+    log_params: cm_calcdata.LogMainParams,
     log_costs: oz_calcdata.OzLogFbsCosts,
 ) -> Decimal:
     """Ozon FBS logistics fee calculator
@@ -12,7 +14,7 @@ def oz_log_fbs_clc(
       - box volume
       - fbs logistics costs
 
-    :param item_params: An instance of OzLogItemParams dataclass
+    :param log_params: An instance of LogMainParams dataclass
     :param log_costs: An instance of OzLogFbsCosts dataclass
     :return: Logistics fee value
     """
@@ -21,25 +23,25 @@ def oz_log_fbs_clc(
     MIN_LIMIT = 0.4
     AVG_LIMIT = 1
     # Calulate max volume
-    if item_params.box_volume >= MAX_LIMIT:
-        return log_costs.fix_large_fbs * item_params.local_index
+    if log_params.box_volume >= MAX_LIMIT:
+        return log_costs.fix_large_fbs * log_params.local_index
     # Calculate < 0.4l volume
-    if item_params.box_volume <= MIN_LIMIT:
-        return log_costs.minimal_price_fbs * item_params.local_index
+    if log_params.box_volume <= MIN_LIMIT:
+        return log_costs.minimal_price_fbs * log_params.local_index
     # Calculate 0.4-1l volume
-    if item_params.box_volume <= AVG_LIMIT:
-        return log_costs.base_price_fbs * item_params.local_index
+    if log_params.box_volume <= AVG_LIMIT:
+        return log_costs.base_price_fbs * log_params.local_index
     # Calculate > 1l volume
     add_volume_price = (
-        math.ceil(item_params.box_volume - 1) * log_costs.volume_factor_fbs
+        math.ceil(log_params.box_volume - 1) * log_costs.volume_factor_fbs
     )
     return (
         add_volume_price + log_costs.base_price_fbs
-    ) * item_params.local_index
+    ) * log_params.local_index
 
 
 def oz_log_fbo_clc(
-    item_params: oz_calcdata.OzLogItemParams,
+    log_params: cm_calcdata.LogMainParams,
     log_costs: oz_calcdata.OzLogFboCosts,
 ) -> Decimal:
     """Ozon FBO logistics fee calculator
@@ -47,7 +49,7 @@ def oz_log_fbo_clc(
       - box volume
       - fbo logistics costs
 
-    :param item_params: An instance of OzLogItemParams dataclass
+    :param log_params: An instance of LogMainParams dataclass
     :param log_costs: An instance of OzLogFboCosts dataclass
     :return: Logistics fee value
     """
@@ -55,22 +57,22 @@ def oz_log_fbo_clc(
     MAX_LIMIT = 190
     AVG_LIMIT = 1
     # Calulate max volume
-    if item_params.box_volume >= MAX_LIMIT:
-        return log_costs.fix_large_fbo * item_params.local_index
+    if log_params.box_volume >= MAX_LIMIT:
+        return log_costs.fix_large_fbo * log_params.local_index
     # Calculate =< 1l volume
-    if item_params.box_volume <= AVG_LIMIT:
-        return log_costs.base_price_fbo * item_params.local_index
+    if log_params.box_volume <= AVG_LIMIT:
+        return log_costs.base_price_fbo * log_params.local_index
     # Calculate > 1l volume
     add_volume_price = (
-        math.ceil(item_params.box_volume - 1) * log_costs.volume_factor_fbo
+        math.ceil(log_params.box_volume - 1) * log_costs.volume_factor_fbo
     )
     return (
         add_volume_price + log_costs.base_price_fbo
-    ) * item_params.local_index
+    ) * log_params.local_index
 
 
 def oz_returns_clc(
-    returns_params: oz_calcdata.OzReturnsParams,
+    returns_params: cm_calcdata.ReturnsParams,
     logistics_fee: Decimal,
     reverse_logistics_fee: Decimal,
 ) -> Decimal:
@@ -81,7 +83,7 @@ def oz_returns_clc(
       - logistics fee value
       - reverse logistics fee value
 
-    :param returns_params: An instance of OzReturnsParams dataclass
+    :param returns_params: An instance of ReturnsParams dataclass
     :return: Returns fee value
     """
     # calculate all nonredemptions processing cost
