1c9861d2d574850f63fa2c7f90e212e51699f040
romz987pc <eesistance@gmail.com>
2025-09-16 10:49:14 +0300

    fix: remove re for box_size and corresponding imports;

--- src/calc/core/domain/enums.py
+++ src/calc/core/domain/enums.py
@@ -1,8 +1,4 @@
 from enum import Enum
-from typing import Annotated
-from pydantic import Field
-
-BoxSize = Annotated[str, Field(pattern=r"^\d{1,3}\*\d{1,3}\*\d{1,3}$")]
 
 
 class LogOptionEnum(Enum):
