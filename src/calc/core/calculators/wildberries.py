3a3ab14233d962e92731f76a6a0ac76060887b69
romz987pc <eesistance@gmail.com>
2025-11-09 00:30:51 +0300

    fix: wildberries logistics calculator according new calculation logic;

--- src/calc/core/calculators/wildberries.py
+++ src/calc/core/calculators/wildberries.py
@@ -1,4 +1,3 @@
-import math
 from decimal import Decimal
 
 # dataclasses
@@ -19,15 +18,33 @@ def wb_log_clc(
     :return: Logistics fee value
     """
     # Constants
+    MIN_LIMIT_1 = 0.2
+    MIN_LIMIT_2 = 0.4
+    MIN_LIMIT_3 = 0.6
+    MIN_LIMIT_4 = 0.8
     AVG_LIMIT = 1
-    # Calculate < 1l volume
+    # Calculate 0.001-0.2l
+    if log_params.box_volume <= MIN_LIMIT_1:
+        return log_costs.min_lim_1_price * log_params.local_index
+    # Calculate 0.2-0.4l
+    if log_params.box_volume <= MIN_LIMIT_2:
+        return log_costs.min_lim_2_price * log_params.local_index
+    # Calculate 0.4-0.6l
+    if log_params.box_volume <= MIN_LIMIT_3:
+        return log_costs.min_lim_3_price * log_params.local_index
+    # Calculate 0.6-0.8l
+    if log_params.box_volume <= MIN_LIMIT_4:
+        return log_costs.min_lim_4_price * log_params.local_index
+    # Calculate 0.8-1l
     if log_params.box_volume <= AVG_LIMIT:
-        return log_costs.base_price * log_params.local_index
-    # Calculate > 1l volume
-    add_volume_price = (
-        math.ceil(log_params.box_volume - 1) * log_costs.volume_factor
-    )
-    return (add_volume_price + log_costs.base_price) * log_params.local_index
+        return log_costs.min_lim_5_price * log_params.local_index
+    # Calculate > 1l
+    additional_volume_price = (
+        log_params.box_volume - 1
+    ) * log_costs.volume_factor
+    return (
+        additional_volume_price + log_costs.base_price
+    ) * log_params.local_index
 
 
 def wb_returns_clc(
